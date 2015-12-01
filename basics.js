3992ac96a70c4a7f5d61cb3b9fb8155220730063
RizOrt218 <rizhiafalen@gmail.com>
2015-11-30 19:15:40 -1000

    completed first assignment

--- basics.js
+++ basics.js
@@ -1,24 +1,57 @@
 /* Create a `myName` variable and assign it a String value */
-
+var myName = ('Dev League');
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+var person = {
+  name : ('Dev League'),
+  age : 12
+};
+
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
 
+ function greet(name){
+  console.log('Hello, my name is ' + person.name);
+  return ('Hello, my name is ' + person.name);
+ }
+
 /* Create a variable called `canDrive`,
- * if it should be true if your person object is at least 16 years old
+ * 'canDrive' should be true if your person object is at least 16 years old
  */
+
+function driving(perAge){
+  if ( perAge >= 16){
+    return true;
+  } else {
+    return false;
+  }
+ }
+
+var canDrive = driving(person.age);
+
 
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
 
+var dataTypes = [true, "", 4, undefined, [], null];
+
 /* Create a Dog object
  * it should have a `bark` function that makes your dog bark!
  * It should also have a name attribute with the value of 'Spot'
  */
+var dog = {
+  name : ('Spot'),
+  bark : function(){
+    console.log('woof');
+    return 'woof';
+  }
+
+};
+
+console.log(dog.name.bark);
